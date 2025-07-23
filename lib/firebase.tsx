a7028b20e63e4d1a4520f024d649c79de3ad701e
Tom Slee <slee.tom@gmail.com>
2025-07-23 15:04:02 -0400

    Updated

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -1,7 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./config";
+//import { firebaseConfig } from "./config";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBau3dXFlmkCcYQxLnz1TGSgUEw3BuH-nY",
+  authDomain: "gig-council.firebaseapp.com",
+  projectId: "gig-council",
+  storageBucket: "gig-council.firebasestorage.app",
+  messagingSenderId: "352321490111",
+  appId: "1:352321490111:web:49a7d8acc3f9bc11c50a0a"
+};
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
