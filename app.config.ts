a1d421412205ee13a3f569b2bfa9056c027b2738
Tom Slee <slee.tom@gmail.com>
2025-08-06 12:15:09 -0400

    fixed up app.config.ts for eas builds

--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "gig-council", // Update this
   slug: "gig-council", // Update this
   extra: {
+    eas: {
+      projectId: "88ce4772-2bf3-4c9d-a4c4-aa54d278be72"
+    },
     firebaseEmail: process.env.FIREBASE_EMAIL,
     firebasePassword: process.env.FIREBASE_PASSWORD,
   },
